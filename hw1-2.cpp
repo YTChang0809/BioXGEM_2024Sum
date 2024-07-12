7d949b0eddedec69cd1405e8061e1fa3ea8f4232
YTCHang0809 <stu110304005.ls10@nycu.edu.tw>
2024-07-12 14:40:46 +0800

    hw1-2

--- hw1-2.cpp
+++ hw1-2.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main(){
     char name[10];
     char gs[20];
+    char s,end;
+    s=' ';
+    end = '|';
+    int m;
+    int n;
     printf("Enter your name: ");
     scanf("%s", &name);
+    ("%lu\n", strlen(name));
     cin.ignore();
     printf("Enter your graduation school: ");
     scanf("%s", &gs);
-    printf("\n+---------------------------+\n");
-    printf("|%16s           |\n", name);
-    printf("|%18s         |\n", gs);
-    printf("+---------------------------+");
+    printf("%lu\n", strlen(gs));
+    m = (30 - strlen(name)) / 2 ;
+    n = (30 - strlen(gs)) / 2;
+    
+
+
+    printf("\n+------------------------------+\n");
+    printf("|%*c%s%*c%c\n",m,s,name,m,s,end);
+    printf("|%*c%s%*c%c\n",n,s, gs,n,s,end);
+    printf("+------------------------------+");
 
     return 0;
 }
